affe64992891aaa947a3ee68835e6460c9801452
pangkunyi <pangkunyi@gmail.com>
2015-07-01 18:17:48 +0800

    add logs

---
+++ logs/logs.go
@@ -0,0 +1,54 @@
+package logs
+
+import (
+	"fmt"
+	"os"
+	"sync"
+	"time"
+)
+
+type Logger struct {
+	fd       *os.File
+	mu       sync.Mutex
+	filename string
+	rotate   bool
+	logDay   int
+}
+
+func NewLogger(filename string, rotate bool) *Logger {
+	return &Logger{filename: filename, rotate: rotate, logDay: -1}
+}
+
+func (l *Logger) checkLogFile() error {
+	now := time.Now()
+	if l.fd != nil {
+		if !l.rotate || l.logDay == now.Day() {
+			return nil
+		}
+		l.fd.Close()
+	}
+	filename := l.filename
+	if l.rotate {
+		filename = fmt.Sprintf(l.filename, now.Format("2006-01-02"))
+	}
+	fd, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0660)
+	if err != nil {
+		return err
+	}
+	l.fd = fd
+	return err
+}
+
+func (l *Logger) Print(msg string) error {
+	l.mu.Lock()
+	defer l.mu.Unlock()
+	if err := l.checkLogFile(); err != nil {
+		return err
+	}
+	_, err := l.fd.Write([]byte(msg))
+	return err
+}
+
+func (l *Logger) Printf(format string, v ...interface{}) {
+	l.Print(fmt.Sprintf(format, v...))
+}
---
+++ logs/logs_test.go
@@ -0,0 +1,27 @@
+package logs
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	runtime.GOMAXPROCS(5)
+	logger := NewLogger("/tmp/a.%s.log", true)
+	var wg sync.WaitGroup
+	for j := 0; j < 100; j++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			for i := 0; i < 10000; i++ {
+				err := logger.Print(fmt.Sprintf("%d\x01sdkfjsdkfjs\x01ksdfjk\x01ksdfksdf\n", j*1000+i))
+				if err != nil {
+					t.Errorf("failed:%s", err)
+				}
+			}
+		}(j)
+	}
+	wg.Wait()
+}

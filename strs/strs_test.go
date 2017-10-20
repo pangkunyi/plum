b71cda85da88eaae82f8c6d43bc7716e2a1b4067
pangkunyi <pangkunyi@gmail.com>
2017-10-20 10:49:29 +0800

    add ' for sql escape

--- strs/strs_test.go
+++ strs/strs_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestLog(t *testing.T) {
-	ori := "a\n" + "\\" + "\r" + "\u0000" + "\"" + "\u0032" + "\u00a5" + "\u20a9"
-	expected := "a\\n" + "\\\\" + "\\r" + "\\0" + "\\\"" + "\\Z" + "" + ""
+	ori := "a\n" + "'" + "\\" + "\r" + "\u0000" + "\"" + "\u0032" + "\u00a5" + "\u20a9"
+	expected := "a\\n" + "\\'" + "\\\\" + "\\r" + "\\0" + "\\\"" + "\\Z" + "" + ""
 	if expected != strs.SqlEscape(ori) {
 		t.Errorf("sql escape failed[%v]-[%v]", ori, strs.SqlEscape(ori))
 	}
